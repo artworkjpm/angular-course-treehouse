ddd968a9cdc80cc9567feddd837d2ed17edcc407
John Moran <johnnymoran44@hotmail.com>
2019-07-14 12:30:57 +0200

    xxxx

--- photo_blog/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-root",
-  template: `
-    <h2>hi john</h2>
-  `
-})
-export class AppComponent {}
--- photo_blog/wwwroot/app.549531d6bfc34330aac7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
-//# sourceMappingURL=app.549531d6bfc34330aac7.js.map
